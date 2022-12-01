5816e2c26a38e8761886698479b98ec9999400a5
borisp <boris.pigin@gmail.com>
2022-12-01 20:27:02 +0100

    LineToNumber: specification consumption type

---
+++ src/main/java/org/tekeli/borisp/adventcode2022/day01/LineToNumberImpl.java
@@ -0,0 +1,8 @@
+package org.tekeli.borisp.adventcode2022.day01;
+
+public class LineToNumberImpl implements LineToNumber {
+    @Override
+    public void apply(final String line) {
+
+    }
+}
--- src/test/java/org/tekeli/borisp/adventcode2022/day01/LineToNumberTest.java
+++ src/test/java/org/tekeli/borisp/adventcode2022/day01/LineToNumberTest.java
@@ -1,6 +1,23 @@
 package org.tekeli.borisp.adventcode2022.day01;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class LineToNumberTest {
 
     private LineToNumber unitUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        unitUnderTest = new LineToNumberImpl();
+    }
+
+    @Test
+    void shouldConsumeString() {
+        final var line = "LINE";
+
+        assertDoesNotThrow(() -> unitUnderTest.apply(line));
+    }
 }
