e6e6c5b759201e100261c621485cbdf593d81275
borisp <boris.pigin@gmail.com>
2022-12-01 19:32:33 +0100

    initial

---
+++ src/main/java/org/tekeli/borisp/adventcode2022/day01/Main.java
@@ -0,0 +1,7 @@
+package org.tekeli.borisp.adventcode2022.day01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Advent Code 2022 Day 01!");
+    }
+}
